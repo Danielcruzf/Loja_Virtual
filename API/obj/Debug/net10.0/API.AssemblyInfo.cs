320e2e653f3482242e0ddc01ddacf0a55ab67b05
Carlos Daniel Cruz <carlosdanielcruz57@gmail.com>
2025-04-15 13:25:42 -0300

    Criação do Babco de dados e instalaçoa do extensao sqlite viewer no vscode para visualização das informações do Sqlite

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b9c372ab005023403dc35367d9e0d66aae8f47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1addd235863050f99f5d903ea45d0253a031070d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
