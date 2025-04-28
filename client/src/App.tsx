ed70ee2ef3c42af58bbbd4248baf62fc15dfb418
Carlos Daniel Cruz <carlosdanielcruz57@gmail.com>
2025-04-28 12:30:39 -0300

    treinado o conceito de hooks usando 'useState' para verificar status e adicionado a função 'addProduct' para modificar o status do produto

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
-const products = [
-  { name: 'produto1', price: 100 },
-  { name: 'product 2', price: 200 },
-  { name: 'product 3', price: 300 },
-]
+import { useState } from 'react'
 function App() {
+  const [products, setProducts] = useState([
+    { name: 'product 1', price: 100 },
+    { name: 'product 2', price: 200 },
+    { name: 'product 3', price: 300 }
+  ])
+  // o useState é um hook que permite adicionar estado a um componente funcional
+  // o useState retorna um array com dois elementos, o primeiro é o estado atual e o segundo é uma função para atualizar o estado.
+  const addProduct =() => {setProducts(prevState=>[...prevState, { name: 'product '+(prevState.length+1), price:(prevState.length*100)+100}])}
 
   return (
     <div>
@@ -12,6 +16,7 @@ function App() {
         {products.map((item, index)=> (
           <li key={index}>{item.name} - {item.price}</li>))}
       </ul>
+      <button onClick={addProduct}>Adicionar produto</button>
 
     </div>
   )
