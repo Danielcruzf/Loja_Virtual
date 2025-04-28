e4006c888ef2e7f6d6d20a0a07db04a3b8531c05
Carlos Daniel Cruz <carlosdanielcruz57@gmail.com>
2025-04-28 11:32:34 -0300

    adicionado itens de teste e chave única para evitar duplicidade na renderização dos itens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,23 @@
+const products = [
+  { name: 'produto1', price: 100 },
+  { name: 'product 2', price: 200 },
+  { name: 'product 3', price: 300 },
+]
 function App() {
-  
+
   return (
-    <div style={{fontSize: '1.6rem'}}>ReStore</div>
+    <div>
+      <h1>ReStore</h1>
+      <ul>
+        {products.map((item, index)=> (
+          <li key={index}>{item.name} - {item.price}</li>))}
+      </ul>
+
+    </div>
   )
 }
 
 export default App
+// todos os itens devem ter uma chave(key) unica evitando assim o erro de duplicidade
+// o key deve ser unico para cada item da lista, se o mesmo item for renderizado mais de uma vez, o mesmo key pode ser utilizado
+
