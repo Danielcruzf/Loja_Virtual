a2a2a84e8af273a0e9e449802c61f9241853ba90
carlos daniel <carlosdanielcruz57@gmail.com>
2025-05-22 15:20:31 -0300

    atualização carrinho de compras

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01dcff4df7137d9cb0930ab75ecb8e2c383b7094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8a52c8cb110fe2ec5c796992ad9d8bdba555200")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
