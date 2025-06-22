eae0f5bbc0c200aa613f018d991e9e4e55131e59
Daniel cruz <carlosdanielcruz57@gmail.com>
2025-06-22 12:38:35 -0300

    finalização do capitulo 9 - Identity

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,4 @@
-using System;
+
 using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -6,87 +6,91 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class AccountController(SignInManager<User> signInManager) : BaseApiController
 {
-    public class AccountController : BaseApiController
+    [HttpPost("register")]
+    public async Task<ActionResult> RegisterUser([FromBody] RegisterDto registerDto)
     {
-        private readonly SignInManager<User> signInManager;
+        var user = new User { UserName = registerDto.Email, Email = registerDto.Email };
 
-        public AccountController(SignInManager<User> signInManager)
+        var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
+
+        if (!result.Succeeded)
         {
-            this.signInManager = signInManager;
-        }
-
-        [HttpPost("register")]//Resumindo: esse método cadastra um novo usuário, valida possíveis erros e, se tudo der certo, adiciona o usuário ao grupo "Member".
-        public async Task<ActionResult> RegisterUser(RegisterDto registerDto)
-        {
-            var user = new User { UserName = registerDto.Email, Email = registerDto.Email };//cria um novo usuário com o nome de usuário e email fornecidos no DTO de registro.
-
-            var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);//
-
-            if (!result.Succeeded)
+            foreach (var error in result.Errors)
             {
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(error.Code, error.Description);
-                }
-                return ValidationProblem();
+                ModelState.AddModelError(error.Code, error.Description);
             }
-            await signInManager.UserManager.AddToRoleAsync(user, "Member");//Se o registro for bem-sucedido, adiciona o novo usuário ao papel (role) "Member".
 
-            return Ok();
+            return ValidationProblem();
         }
 
-        [HttpGet("user-info")]
-        public async Task<ActionResult> GetUserInfo()
+        await signInManager.UserManager.AddToRoleAsync(user, "Member");
+
+        return Ok();
+    }
+
+    [HttpGet("user-info")]
+    public async Task<ActionResult> GetUserInfo()
+    {
+        if (User.Identity?.IsAuthenticated == false) return NoContent();
+
+        var user = await signInManager.UserManager.GetUserAsync(User);
+
+        if (user == null) return Unauthorized();
+
+        var roles = await signInManager.UserManager.GetRolesAsync(user);
+
+        return Ok(new
         {
-            if (User.Identity?.IsAuthenticated ?? false) return NoContent();//verificação se o usuário está autenticado.
+            user.Email,
+            user.UserName,
+            Roles = roles
+        });
+    }
 
-            var user = await signInManager.UserManager.GetUserAsync(User);//Obtém o usuário autenticado.
-            if (user == null) return Unauthorized();//Se o usuário não for encontrado, retorna um erro de autorização.
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
 
-            var roles = await signInManager.UserManager.GetRolesAsync(user);//Obtém os papéis (roles) do usuário autenticado.
-            return Ok(new { user.UserName, user.Email, Roles = roles });//Retorna as informações do usuário
-        }
+        return NoContent();
+    }
 
-        [HttpPost("logout")]
-        public async Task<ActionResult> Logout()
-        {
-            await signInManager.SignOutAsync();
+    [Authorize]
+    [HttpPost("address")]
+    public async Task<ActionResult<Address>> CreateOrUpdateAddress(Address address)
+    {
+        var user = await signInManager.UserManager.Users
+             .Include(u => u.Address)
+             .FirstOrDefaultAsync(u => u.UserName == User.Identity!.Name);
 
-            return NoContent();
-        }
-[Authorize]
-        [HttpPost("address")]
-        public async Task<ActionResult<Address>> CreateOrUpdateAddress(Address address)
-        {
-            var user = await signInManager.UserManager.Users
-      .Include(x => x.Address)
-      .FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+        if (user == null) return Unauthorized();
 
-            if (user == null) return Unauthorized();
+        user.Address = address;
 
-            user.Address = address;
+        var result = await signInManager.UserManager.UpdateAsync(user);
 
-            var result = await signInManager.UserManager.UpdateAsync(user);
+        if (!result.Succeeded) return BadRequest("Problem updating user address");
 
-            if (!result.Succeeded) return BadRequest("Problem updatating user address");
+        return Ok(user.Address);
 
-            return Ok(user.Address);
-        }
-        [Authorize]
-        [HttpGet("address")]
-        public async Task<ActionResult<Address>> GetSavedAddress()
-        {
-            var address = await signInManager.UserManager.Users
-            .Where(x => x.UserName == User.Identity!.Name)
-            .Select(x => x.Address)
+    }
+
+    [Authorize]
+    [HttpGet("address")]
+    public async Task<ActionResult<Address>> GetSavedAddress()
+    {
+        var address = await signInManager.UserManager.Users
+            .Where(u => u.UserName == User.Identity!.Name)
+            .Select(u => u.Address)
             .FirstOrDefaultAsync();
 
-            if (address == null) return NoContent();
+        if (address == null) return NoContent();
 
-            return address;
-        }
+        return address;
     }
-}
 
+}
