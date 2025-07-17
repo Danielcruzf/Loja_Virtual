08f59ea0c9b584d31c35ddf1556c19f1f680e260
DanielCruz <carlosdanielcruz57@gmail.com>
2025-07-16 23:18:33 -0300

    configuração do CRUD

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -76,9 +76,25 @@ namespace API.Controllers
             var result = await context.SaveChangesAsync() > 0;
 
             if (result) return NoContent();
-            
+
             return BadRequest("Problem updating product");
         }
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
 
+            if (product == null) return NotFound();
+
+            context.Products.Remove(product);
+
+            var result = await context.SaveChangesAsync() > 0;
+
+            if (result) return Ok();
+
+            return BadRequest("Problem deleting product");
+
+        }
     }
 }
